280595f9a0160deb7f6ab279cc188b775a725731
jguilhem <julien.guilhemsans@capgemini.com>
2018-02-21 18:15:50 +0100

    suite dev appli

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { ItemsModule } from './items/items.module';
 
 
 @NgModule({
@@ -14,8 +17,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CoreModule,
     HomeModule,
+    ItemsModule,
+    NgbModule.forRoot(),
     PageNotFoundModule,
-    SharedModule,
+    SharedModule
   ],
   declarations: [
     AppComponent
---
+++ my-app/src/app/core/services/collection.ts
@@ -0,0 +1,22 @@
+import { Item } from '../../shared/interfaces/item.model';
+
+export const COLLECTION: Item[] = [
+  {
+    id: 'a1',
+    name: 'Christophe',
+    reference: '1234',
+    state: 'A livrer'
+  },
+  {
+    id: 'b1',
+    name: 'Julien',
+    reference: '2413',
+    state: 'En cours'
+  },
+  {
+    id: 'c1',
+    name: 'Charlotte',
+    reference: '2537',
+    state: 'livrée'
+  }
+];
