1b49d63831ca1229d8a9548d6e2776a149cac94a
jguilhem <julien.guilhemsans@capgemini.com>
2018-02-22 11:21:06 +0100

    suite dev matin

--- my-app/src/app/core/services/collection.ts
+++ my-app/src/app/core/services/collection.ts
@@ -1,22 +1,23 @@
 import { Item } from '../../shared/interfaces/item.model';
+import { State } from '../../shared/enums/state.enum';
 
 export const COLLECTION: Item[] = [
   {
     id: 'a1',
     name: 'Christophe',
     reference: '1234',
-    state: 'A livrer'
+    state: State.ALIVRER
   },
   {
     id: 'b1',
     name: 'Julien',
     reference: '2413',
-    state: 'En cours'
+    state: State.ENCOURS
   },
   {
     id: 'c1',
     name: 'Charlotte',
     reference: '2537',
-    state: 'livrée'
+    state: State.LIVREE
   }
 ];
