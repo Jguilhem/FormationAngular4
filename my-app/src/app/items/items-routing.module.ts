65b49ac7b5fca523ec89748d5ac6328b2bf9449d
jguilhem <julien.guilhemsans@capgemini.com>
2018-02-23 14:34:28 +0100

    dev debut aprem j3

--- my-app/src/app/items/items-routing.module.ts
+++ my-app/src/app/items/items-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ListItemsComponent } from './containers/list-items/list-items.component';
+import { FormComponent } from '../shared/components/form/form.component';
 
 const appRoutes: Routes = [
   { path: 'liste', component: ListItemsComponent },
+  { path: 'add', component: FormComponent }
 ];
 
 @NgModule({
